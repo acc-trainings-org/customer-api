bf00a19d63fe39e3e39d6a4ffcc4a7b2ead7ed0e
Marina Orzechowski <m.skachko@hotmail.com>
2022-06-14 19:31:03 -0400

    Initial Spring project setup
    - Sets up a new Spring project
    - Uses .yml file to auto generate api
    - Creates customer controller and required repository and service.

---
+++ src/main/java/com/acc/training/customerapi/service/CustomerService.java
@@ -0,0 +1,25 @@
+package com.acc.training.customerapi.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.acc.training.customerapi.model.Customer;
+import com.acc.training.customerapi.repository.CustomerRepository;
+
+@Service
+public class CustomerService {
+
+    @Autowired
+    private CustomerRepository repository;
+    
+    public Customer getCustomer (String id) {
+
+        return repository.getCustomer(id);
+    }
+
+    public Customer createCustomer (Customer customer) {
+
+        return repository.saveCustomer(customer);
+    }
+}
+
