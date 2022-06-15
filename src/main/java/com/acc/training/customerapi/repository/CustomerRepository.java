9ace2b77197f9cece21778d8da1e4a78aa895550
lloydbriantech <lloydbriantech@outlook.com>
2022-06-14 21:19:37 -0400

    Day june14 first checkin to personal branch

---
+++ src/main/java/com/acc/training/customerapi/repository/CustomerRepository.java
@@ -0,0 +1,38 @@
+package com.acc.training.customerapi.repository;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Repository;
+
+import com.acc.training.customerapi.model.Customer;
+
+@Repository
+public class CustomerRepository {
+    
+    private static final Map<String,Customer> customerDB = new HashMap<>();
+
+    static {
+        initCustomerDB();
+    }
+    
+    private static void initCustomerDB() {
+        Customer cust1 = new Customer();
+        cust1.setCustomerId("12345");
+        cust1.setCustomerName("Lloyd Dagoc");
+        cust1.setCustomerAddress("New Hampshire, USA");
+        cust1.setOfficeCode(BigDecimal.valueOf(045));
+        customerDB.put(cust1.getCustomerId(), cust1);
+    }
+    
+    public Customer getCustomer(String id){
+        return customerDB.get(id);
+    }
+
+    public Customer saveCustomer(Customer customerObj){
+        customerDB.put(customerObj.getCustomerId(), customerObj);
+        return customerDB.get(customerObj.getCustomerId());
+    }    
+
+}
