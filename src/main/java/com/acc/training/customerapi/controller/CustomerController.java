baf9543dbdcc3dcc6ade0ed8a5419fe0aab9dc57
MalikDevDSGN <110063281+MalikbDevDSGN@users.noreply.github.com>
2024-02-08 18:45:00 -0500

    Updated code.
    Code was able to run

--- src/main/java/com/acc/training/customerapi/controller/CustomerController.java
+++ src/main/java/com/acc/training/customerapi/controller/CustomerController.java
@@ -11,30 +11,36 @@ import com.acc.training.customerapi.api.CustomerApi;
 import com.acc.training.customerapi.model.Customer;
 import com.acc.training.customerapi.service.CustomerService;
 
+
 @RestController
-public class CustomerController implements CustomerApi {
+public class CustomerController implements CustomerApi{
 
-  @Autowired
-  public CustomerService service;
+    @Autowired
+    public CustomerService service;
 
-  @Override
-  public ResponseEntity<Customer> createCustomer(@Valid Customer body) {
-    Customer response = service.createCustomer(body);
-    return ResponseEntity.status(HttpStatus.OK).body(response);
-  }
-
-  @Override
-  public ResponseEntity<Customer> getCustomer(String id) {
-
-    Customer response = service.getCustomer(id);
-
-    if (response == null) {
-      return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    @Override
+    public ResponseEntity<Customer> createCustomer(@Valid Customer body) {
+        Customer response = service.createCustomer(body);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
-    //
+    @Override
+    public ResponseEntity<Customer> getCustomer(String id) {
 
-    return ResponseEntity.status(HttpStatus.OK).body(response);
-  }
+         Customer response = service.getCustomer(id);
+         
 
+
+
+         if(response == null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+         }
+
+        //  
+
+      return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    
+ 
 }
