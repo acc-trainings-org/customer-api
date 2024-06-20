6ea9321c59b92d7ffb12e0febcc89dcb55409939
bhanu-rakesh <bhanurakesh2208@gmail.com>
2024-06-20 08:28:51 +0530

    my Initial Commit for customer-api code
    First basic functionality

---
+++ src/main/java/com/acc/training/customer_api/controller/CustomerController.java
@@ -0,0 +1,36 @@
+package com.acc.training.customer_api.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.acc.training.customer_api.service.CustomerService;
+import com.acc.training.customerapi.api.CustomerApi;
+import com.acc.training.customerapi.model.Customer;
+
+@RestController
+public class CustomerController implements CustomerApi{
+
+    @Autowired
+    private CustomerService customerService;
+
+
+    @Override
+    public ResponseEntity<Customer> createCustomer(@Valid Customer body) {
+        Customer response = customerService.createCustomer(body);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @Override
+    public ResponseEntity<Customer> getCustomer(String id) {
+        Customer response = customerService.getCustomer(id);
+        if(response==null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+    
+}
