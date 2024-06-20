f43ac0366663b526b842530b4aa87575edae4f9a
Aashi1205 <80957901+Aashi1205@users.noreply.github.com>
2024-06-20 10:34:25 +0530

    Committing code till session 6/20
    Committing code till session 6/20

---
+++ src/main/java/com/acc/training/customer_api/service/CustomerService.java
@@ -0,0 +1,30 @@
+package com.acc.training.customer_api.service;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Service;
+
+import com.acc.training.customer_api.repository.CustomerRepository;
+import com.acc.training.customerapi.model.Customer;
+
+@Service
+public class CustomerService {
+
+    @Autowired
+    private CustomerRepository repository;
+
+    public Customer createCustomer(@Valid Customer body) {
+        return repository.saveCustomer(body);
+    }
+
+    /**
+     * @param id
+     * @return
+     */
+    public Customer getCustomer(String id) {
+        return repository.fetchCustomer(id);
+    }
+
+}
