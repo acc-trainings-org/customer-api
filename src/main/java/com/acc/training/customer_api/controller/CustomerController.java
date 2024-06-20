2b5c15d8c09788668268b796024955dcbbc2dffb
harish.javap@gmail.com <harish.chinthapalli@accenture.com>
2024-06-20 08:51:33 +0530

    Initial commit for customerAPI

---
+++ src/main/java/com/acc/training/customer_api/controller/CustomerController.java
@@ -0,0 +1,41 @@
+package com.acc.training.customer_api.controller;
+
+import javax.validation.Valid;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.acc.training.customer_api.service.CustomerService;
+import com.acc.training.customerapi.api.CustomerApi;
+import com.acc.training.customerapi.model.Customer;
+
+
+@RestController
+public class CustomerController implements CustomerApi{
+
+
+    @Autowired
+    private CustomerService service;
+
+    @Override
+    public ResponseEntity<Customer> createCustomer(@Valid Customer body) {
+        Customer response =service.createCustomer(body);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @Override
+    public ResponseEntity<Customer> getCustomer(String id) {
+        Customer response = service.getCustomer(id);
+
+        if(null == response){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    
+}
