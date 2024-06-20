a3ee04560ed5ba7fe84e6dff211a059642f4317b
skjain246 <saurabhjain0714@gmail.com>
2024-06-20 08:28:24 +0530

    my initial commit customerAPI code

---
+++ customer-api/src/main/java/com/acc/training/customer_api/repository/CustomerRepository.java
@@ -0,0 +1,42 @@
+package com.acc.training.customer_api.repository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Repository;
+
+import com.acc.training.customerapi.model.Customer;
+
+@Repository
+public class CustomerRepository {
+
+    private static final Map<String, Customer>customerDB = new HashMap<>();
+
+    static{
+        
+        iniCustomerDB();
+    }
+
+    public Customer saveCustomer(@Valid Customer body) {
+        customerDB.put(body.getCustomerId(), body);
+       return customerDB.get(body.getCustomerId());
+    }
+
+    
+
+    public Customer fetchCustomer(String id) {
+         return customerDB.get(id);
+    }
+
+    private static void iniCustomerDB() {
+        
+        Customer customer = new Customer();
+        customer.setCustomerId("12345");
+        customer.setCustomerName("TestName");
+        customer.setCustomerAddress("kharadi, Pune");
+        customerDB.put(customer.getCustomerId(), customer);
+    }
+    
+}
