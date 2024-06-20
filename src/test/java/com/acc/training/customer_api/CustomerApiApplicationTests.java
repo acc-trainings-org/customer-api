e647e8daa4ad66d3a31a9444d68e16409d39ce33
rahul0309 <rahul.a0309@gmail.com>
2024-06-19 22:55:44 -0400

    my first code

---
+++ src/test/java/com/acc/training/customer_api/CustomerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acc.training.customer_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
