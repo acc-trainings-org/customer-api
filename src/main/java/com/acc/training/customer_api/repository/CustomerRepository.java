c3e2d6860a53dce2c319aac423a0e4b1329ba8b4
adityajdeshpande <adityajd.mtech@gmail.com>
2024-06-21 18:00:18 +0530

    MongoDB changes for Customer API

--- src/main/java/com/acc/training/customer_api/repository/CustomerRepository.java
+++ src/main/java/com/acc/training/customer_api/repository/CustomerRepository.java
@@ -1,40 +1,9 @@
 package com.acc.training.customer_api.repository;
 
-import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import com.acc.training.customer_api.domain.CustomerDomain;
 
-import org.springframework.stereotype.Repository;
+public interface CustomerRepository extends MongoRepository<CustomerDomain, String> {
 
-import com.acc.training.customerapi.model.Customer;
-
-@Repository
-public class CustomerRepository {
-
-    private static final Map<String,Customer> customerDB = new HashMap<>();
-
-    static{
-        initCustomerDB();
-    }
-
-    private static void initCustomerDB() {
-        
-        Customer cust = new Customer();
-        cust.setCustomerId("12345");
-        cust.setCustomerName("Aditya");
-        cust.setCustomerAddress("India");
-        cust.setOfficeCode(845);
-
-        customerDB.put(cust.getCustomerId(), cust);
-    }
-
-    public Customer saveCustomer(Customer body) {
-        customerDB.put(body.getCustomerId(), body);
-        return customerDB.get(body.getCustomerId());
-    }
-
-    public Customer fetchCustomer(String id) {
-        return customerDB.get(id);
-    }
-    
+    CustomerDomain findByCustomerId(String customerId);
 }
--- src/main/java/com/acc/training/customer_api/service/CustomerService.java
+++ src/main/java/com/acc/training/customer_api/service/CustomerService.java
@@ -1,8 +1,11 @@
 package com.acc.training.customer_api.service;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.acc.training.customer_api.domain.CustomerDomain;
 import com.acc.training.customer_api.repository.CustomerRepository;
 import com.acc.training.customerapi.model.Customer;
 
@@ -12,12 +15,33 @@ public class CustomerService {
     @Autowired
     private CustomerRepository customerRepository;
 
-    public Customer createCustomer(Customer body) {
-        return customerRepository.saveCustomer(body);
+    public Customer createCustomer(Customer customer) {
+        return mapDomainToModel(customerRepository.save(mapModelToDomain(customer)));
+    }
+ 
+    public Customer getCustomer(String id) {
+        return mapDomainToModel(customerRepository.findByCustomerId(id));
     }
 
-    public Customer getCustomer(String id) {
-        return customerRepository.fetchCustomer(id);
+    public CustomerDomain mapModelToDomain(Customer customer){
+
+        CustomerDomain customerDomain = new CustomerDomain();
+        customerDomain.setCustomerId(customer.getCustomerId());
+        customerDomain.setCustomerName(customer.getCustomerName());
+        customerDomain.setCustomerAddress(customer.getCustomerAddress());
+        customerDomain.setOfficeCode(customer.getOfficeCode());
+
+        return customerDomain;
     }
     
+    public Customer mapDomainToModel(CustomerDomain customerDomain){
+
+        Customer customer = new Customer();
+        customer.setCustomerId(customerDomain.getCustomerId());
+        customer.setCustomerName(customerDomain.getCustomerName());
+        customer.setCustomerAddress(customerDomain.getCustomerAddress());
+        customer.setOfficeCode(customerDomain.getOfficeCode());
+
+        return customer;
+    }
 }
