30461d95bb3c46e9ebaaf26a2013b690c0ebfab3
kvivek26 <kandukurivivek08@gmail.com>
2024-06-21 07:19:11 +0530

    My first CustomerApi changes

---
+++ src/main/java/com/acc/training/customer_api/CustomerApiApplication.java
@@ -0,0 +1,13 @@
+package com.acc.training.customer_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerApiApplication.class, args);
+	}
+
+}
