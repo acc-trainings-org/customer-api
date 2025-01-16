a1a08f03846d79f9ff30ba28f9235a3b5a298cab
rahul0309 <rahul.a0309@gmail.com>
2025-01-15 19:35:11 -0500

    my first api

---
+++ src/main/java/com/acc/training/customer_api/controller/CustomerController.java
@@ -0,0 +1,42 @@
+package com.acc.training.customer_api.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.acc.training.customer_api.service.CustomerService;
+import com.acc.training.customerapi.api.CustomerApi;
+import com.acc.training.customerapi.model.Customer;
+
+@RestController
+public class CustomerController implements CustomerApi {
+
+    @Autowired
+    private CustomerService service;
+
+    @Override
+    public ResponseEntity<Customer> createCustomer(@Valid Customer body) {
+
+        Customer response = service.createCustomer(body);
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @Override
+    public ResponseEntity<Customer> getCustomer(String id) {
+
+        //TODO: Handle if customerID is less than 5 or greater than 8 - Bad Request
+
+        Customer response = service.getCustomer(id);
+
+        if(null == response){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+    
+}
